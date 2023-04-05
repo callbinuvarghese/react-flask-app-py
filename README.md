61a4108d938f5847c27b1e65f269bfffb613d9e0
callbinuvarghese <call.binu.varghese@gmail.com>
2023-04-05 12:26:24 -0400

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# react-flask-app-py
-React application with Py Flask as the backend in the same container in Cloud Run
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+A Flask based API backend was added in the *api* directory.
+
+Read my [tutorial](https://blog.miguelgrinberg.com/post/how-to-create-a-react--flask-project) on how to create Flask + React combined projects.
---
+++ api/api.py
@@ -0,0 +1,24 @@
+import time
+from flask import Flask
+
+app = Flask(__name__, static_folder='../build', static_url_path='/')
+
+
+@app.errorhandler(404)
+def not_found(e):
+    return app.send_static_file('index.html')
+
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+
+
+@app.route('/api/time')
+def get_current_time():
+    return {'time': time.time()}
+
+@app.route('/api/gethello')
+def get_hello():
+    return {'hello'}
+
